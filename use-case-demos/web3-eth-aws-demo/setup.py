f2a97ac74443434d00e7f7190e5fb2996104dac7
Jaz Ku <jazku@puppygraph.com>
2026-05-19 23:05:30 -0700

    move spark session creation into main

--- use-case-demos/web3-eth-aws-demo/setup.py
+++ use-case-demos/web3-eth-aws-demo/setup.py
@@ -1,20 +1,9 @@
 import argparse
+import os
 from pyspark.sql import SparkSession
-from config import AWS_REGION, TABLES, ETH_SCHEMAS, TARGET_BUCKET, TARGET_DB
+from config import TABLES, ETH_SCHEMAS, TARGET_BUCKET, TARGET_DB
 
-spark = SparkSession.builder \
-    .master("local[*]") \
-    .config("spark.sql.extensions", "org.apache.iceberg.spark.extensions.IcebergSparkSessionExtensions") \
-    .config("spark.sql.catalog.glue_catalog", "org.apache.iceberg.spark.SparkCatalog") \
-    .config("spark.sql.catalog.glue_catalog.type", "glue") \
-    .config("spark.sql.catalog.glue_catalog.warehouse", f"s3://{TARGET_BUCKET}/iceberg/") \
-    .config("spark.sql.catalog.glue_catalog.io-impl", "org.apache.iceberg.aws.s3.S3FileIO") \
-    .config("spark.sql.catalog.glue_catalog.client.region", AWS_REGION) \
-    .config("spark.hadoop.fs.s3a.endpoint", f"s3.{AWS_REGION}.amazonaws.com") \
-    .config("spark.hadoop.fs.s3.impl", "org.apache.hadoop.fs.s3a.S3AFileSystem") \
-    .getOrCreate()
-
-def create_table(table_name):
+def create_table(spark, table_name):
     table_path = f"s3://{TARGET_BUCKET}/iceberg/eth/{table_name}/"
 
     spark.sql(f"CREATE DATABASE IF NOT EXISTS glue_catalog.{TARGET_DB}")
@@ -32,7 +21,7 @@ def create_table(table_name):
     """)
     print(f"{TARGET_DB}.{table_name} created")
 
-def add_files(table_name):
+def add_files(spark, table_name):
     spark.sql(f"""
         CALL glue_catalog.system.add_files(
             table => 'glue_catalog.{TARGET_DB}.{table_name}',
@@ -48,17 +37,31 @@ if __name__ == "__main__":
     parser.add_argument("--add-files", action="store_true")
     args = parser.parse_args()
 
-    if args.create:
-        for table in TABLES:
-            create_table(table)
+    spark = SparkSession.builder \
+        .master("local[*]") \
+        .config("spark.sql.extensions", "org.apache.iceberg.spark.extensions.IcebergSparkSessionExtensions") \
+        .config("spark.sql.catalog.glue_catalog", "org.apache.iceberg.spark.SparkCatalog") \
+        .config("spark.sql.catalog.glue_catalog.type", "glue") \
+        .config("spark.sql.catalog.glue_catalog.warehouse", f"s3://{TARGET_BUCKET}/iceberg/") \
+        .config("spark.sql.catalog.glue_catalog.io-impl", "org.apache.iceberg.aws.s3.S3FileIO") \
+        .config("spark.sql.catalog.glue_catalog.client.region", os.environ["AWS_REGION"]) \
+        .config("spark.hadoop.fs.s3a.endpoint", f"s3.{os.environ['AWS_REGION']}.amazonaws.com") \
+        .config("spark.hadoop.fs.s3.impl", "org.apache.hadoop.fs.s3a.S3AFileSystem") \
+        .getOrCreate()
 
-    if args.add_files:
-        for table in TABLES:
-            try:
-                add_files(table)
+    try:
+        if args.create:
+            for table in TABLES:
+                create_table(spark, table)
 
-            except Exception as e:
-                if "IllegalStateException" in str(e) and "check_duplicate_files" in str(e):
-                    print(f"{table} skipped: partition already loaded")
-                else:
-                    print(f"{table} failed: {e}")
+        if args.add_files:
+            for table in TABLES:
+                try:
+                    add_files(spark, table)
+                except Exception as e:
+                    if "IllegalStateException" in str(e) and "check_duplicate_files" in str(e):
+                        print(f"{table} skipped: partition already loaded")
+                    else:
+                        print(f"{table} failed: {e}")
+    finally:
+        spark.stop()
